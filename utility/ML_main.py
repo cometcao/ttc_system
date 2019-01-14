e3f67212c45841811475450df7162ee078e71021
MetalInvest <MetalInvest@DESKTOP-IARABV4>
2019-01-14 15:39:42 +1100

    update deterministic logic

--- utility/ML_main.py
+++ utility/ML_main.py
@@ -269,12 +269,12 @@ class ML_biaoli_check(object):
                 long_pred = (old_y_class[-1] == -1 and old_long_conf[-1])
                 short_pred = (old_y_class[-1] == 1 and old_short_conf[-1])
                        
-                if check_status:
-                    long_pred = long_pred or (len(old_y_class) >= 2 and old_y_class[-2] == -1 and old_y_class[-1] == 0 and old_long_conf[-1] and old_long_conf[-2]) 
-                    short_pred = short_pred or (len(old_y_class) >= 2 and old_y_class[-2] == 1 and old_y_class[-1] == 0 and old_short_conf[-1] and old_short_conf[-2])
-                else:
-                    long_pred = long_pred or (past_pivot_status == -1 and old_y_class[-1] == 0) 
-                    short_pred = short_pred or (past_pivot_status == 1 and old_y_class[-1] == 0)
+                long_pred = long_pred or (len(old_y_class) >= 2 and old_y_class[-2] == -1 and old_y_class[-1] == 0 and old_long_conf[-1] and old_long_conf[-2]) 
+                short_pred = short_pred or (len(old_y_class) >= 2 and old_y_class[-2] == 1 and old_y_class[-1] == 0 and old_short_conf[-1] and old_short_conf[-2])
+                
+                if check_status and not long_pred and not short_pred: # model not deterministic, we use past pivot point with passive logic
+                    long_pred = long_pred or past_pivot_status == -1
+                    short_pred = short_pred or past_pivot_status == 1
                     
                 if self.isDebug:
                     print(old_pred)
