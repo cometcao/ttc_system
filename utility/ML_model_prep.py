fe8d56a6b90c428f063739c8c4dd1d2ebc6c630c
MLInvestor <MLInvestor@DESKTOP-FMOI76C>
2019-06-18 22:05:52 +0800

    change behaviour

--- utility/ML_model_prep.py
+++ utility/ML_model_prep.py
@@ -322,12 +322,12 @@ class MLDataProcess(object):
 #         print('Test accuracy:', score[1])          
         self.model = model
         
-#         if self.model_name:
-#             if self.saveByte:
-#                 self.save_model_byte(self.model_name, self.model)
-#             else:
-#                 model.save(self.model_name)
-#             print("saved to file {0}".format(self.model_name))       
+        if self.model_name:
+            if self.saveByte:
+                self.save_model_byte(self.model_name, self.model)
+            else:
+                model.save_weights(self.model_name)
+            print("saved weight to file {0}".format(self.model_name))
             
         return min(record.history['val_loss']), max(record.history['val_acc']), min(record.history['loss']), max(record.history['acc'])
         
