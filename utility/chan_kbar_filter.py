fe89ff623680c9c4353add9199ea7191defc5ad1
cometcao <cometcao@gmail.com>
2020-01-18 22:52:50 +0800

    bug fix TYPE I check with relaxed condition

--- utility/chan_kbar_filter.py
+++ utility/chan_kbar_filter.py
@@ -10,12 +10,15 @@ from utility.centralRegion import Chan_Type
 from utility.biaoLiStatus import TopBotType
 from utility.securityDataManager import *
 
+TYPE_III_NUM = 5
+TYPE_I_NUM = 10
+
 def filter_high_level_by_index(period = '5d',direction=TopBotType.top2bot, stock_index='000985.XSHG', df=False):
     all_stocks = JqDataRetriever.get_index_stocks(stock_index)
     result_stocks = []
     for stock in all_stocks:
         stock_high = JqDataRetriever.get_bars(stock, 
-                                               count=8, 
+                                               count=max(TYPE_I_NUM, TYPE_III_NUM), 
                                                end_dt=pd.datetime.now().strftime("%Y-%m-%d %H:%M:%S"), 
                                                unit=period,
                                                fields= ['open',  'high', 'low','close', 'money'], 
@@ -37,9 +40,18 @@ class KBar(object):
         self.close = close
         self.high = high
         self.low = low
+        
+    def __repr__(self):
+        return "\nOPEN:{0} CLOSE:{1} HIGH:{2} LOW:{3}".format(self.open, self.close, self.high, self.low)
 
     @classmethod
-    def contain_zhongshu(cls, first, second, third):
+    def contain_zhongshu(cls, first, second, third, return_core_range=False):
+        '''
+        check consecutive three kbars with intersection, 
+        return_core_range controls return range type, default return core range to increase the robostness of initial filters
+        TYPE III: False
+        TYPE I : True
+        '''
         if first.high >= second.high >= first.low or\
             first.high >= second.low >= first.low or\
             second.high >= first.high >= second.low or\
@@ -49,11 +61,14 @@ class KBar(object):
                 new_high >= third.low >= new_low or\
                 third.high >= new_high >= third.low or\
                 third.high >= new_low >= third.low:
-                return True, max(first.high, second.high, third.high), min(first.low, second.low, third.low)
+                if return_core_range:
+                    return True, min(first.high, second.high, third.high), max(first.low, second.low, third.low)
+                else:
+                    return True, max(first.high, second.high, third.high), min(first.low, second.low, third.low)
         return False, 0, 0
     
     @classmethod 
-    def create_N_kbar(cls, high_df, n = 5):
+    def create_N_kbar(cls, high_df, n = TYPE_III_NUM):
         i = -n
         kbar_list = []
         while i <= -1:
@@ -68,25 +83,25 @@ class KBar(object):
         '''
         max 5 high level KBars
         '''
-        first = kbar_list[-5]
-        second = kbar_list[-4] 
-        third = kbar_list[-3]
-        forth = kbar_list[-2]
-        fifth = kbar_list[-1]
+        first = kbar_list[-TYPE_III_NUM]
+        second = kbar_list[-TYPE_III_NUM+1] 
+        third = kbar_list[-TYPE_III_NUM+2]
+        forth = kbar_list[-TYPE_III_NUM+3]
+        fifth = kbar_list[-TYPE_III_NUM+4]
         result = False
         if fifth.close < fifth.open or fifth.close < (fifth.high + fifth.low)/2.0:
-            check_result, k_m, k_l = cls.contain_zhongshu(first, second, third)
+            check_result, k_m, k_l = cls.contain_zhongshu(first, second, third, return_core_range=False)
             if check_result:
                 result = fifth.low > k_m if direction == TopBotType.top2bot else fifth.high < k_l
             if not result:
-                check_result, k_m, k_l = cls.contain_zhongshu(second, third, forth)
+                check_result, k_m, k_l = cls.contain_zhongshu(second, third, forth, return_core_range=False)
                 result = check_result and fifth.close > k_m if direction == TopBotType.top2bot else fifth.high < k_l 
         return result
     
     @classmethod
     def chan_type_I_check(cls, kbar_list, direction):
         '''
-        Max 8 high level Kbars
+        Max number of high level Kbars
         '''
         result = False
         i = 0
@@ -95,9 +110,9 @@ class KBar(object):
         first_zs_mi = 0
         while i+3 < len(kbar_list):
             first = kbar_list[i]
-            second = kbar_list[i+2]
-            third = kbar_list[i+3]
-            check_result, ma, mi = cls.contain_zhongshu(first, second, third)
+            second = kbar_list[i+1]
+            third = kbar_list[i+2]
+            check_result, ma, mi = cls.contain_zhongshu(first, second, third, return_core_range=True)
             if not first_zs:
                 if check_result:
                     first_zs = True
@@ -108,7 +123,10 @@ class KBar(object):
                     i = i + 1
             else:
                 if check_result:
-                    result == first_zs_ma < mi if direction == TopBotType.bot2top else first_zs_mi > ma
+                    result = first_zs_ma < mi if direction == TopBotType.bot2top else first_zs_mi > ma
+                    if result:
+                        break
+                i = i + 1
         return result
 
     @classmethod
@@ -124,7 +142,7 @@ class KBar(object):
         3. the same kbar or next kbar's return attempt never touch the previous zhongshu (only strong case)
         '''
         result = False
-        num_of_kbar = 5 if chan_type == Chan_Type.III else 8
+        num_of_kbar = TYPE_III_NUM if chan_type == Chan_Type.III else TYPE_I_NUM
         if df:
             if high_df.shape[0] >= 4:
                 i = -num_of_kbar
