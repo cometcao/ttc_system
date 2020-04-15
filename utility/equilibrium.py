97f2f45cee6a40328044bb789ad7ed1260eac277
cometcao <cometcao@gmail.com>
2020-04-15 15:06:41 +0800

    change QvShi_simple logic

--- utility/equilibrium.py
+++ utility/equilibrium.py
@@ -609,8 +609,9 @@ class Equilibrium():
         if zs1.get_level().value >= zs2.get_level().value == zs_level.value:
             [lr1, ur1] = zs1.get_core_region()
             [lr2, ur2] = zs2.get_core_region()
-            if (float_more(lr1, ur2) or float_more(lr2, ur1)):
-#                 (not (self.two_zslx_interact(zs1, zs2) and zslx.isSimple())): # two Zhong Shu without intersection
+            if (float_more(lr1, ur2) or float_more(lr2, ur1)) and\
+                ((not (self.two_zslx_interact(zs1, zs2) and zslx.isSimple())) or\
+                (not zs1.is_complex_type() and self.two_zslx_interact(zs1, zs2) and zslx.isSimple())): # two Zhong Shu without intersection
                 if self.isdebug:
                     print("1 current Zou Shi is QV SHI relaxed \n{0} \n{1}".format(zs1, zs2))
                 relax_result = True
