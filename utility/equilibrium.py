d6dbb2572f714892691712e90b012e5f85010cec
cometcao <cometcao@gmail.com>
2020-06-26 23:08:44 +0800

    we expect less xianduan in beichi

--- utility/equilibrium.py
+++ utility/equilibrium.py
@@ -895,7 +895,9 @@ class Equilibrium():
                 return False
         
         if self.isQvShi and current_chan_type==Chan_Type.I: # BEI CHI
-            if abs(len(a_s) - len(c_s)) >= 4:
+#             abs(len(a_s) - len(c_s)) >= 4:
+            # exhaustion also shows up in number of xianduan
+            if len(a_s) - len(c_s) > 2 or len(a_s) - len(c_s) < 0:
                 if self.isdebug:
                     print("Not matching XD structure")
                 return False
